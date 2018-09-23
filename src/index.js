371d12132c5106ad3f96d32476042a4c2f2a052c
innazl <mozyr.innazl@tut.by>
2018-09-23 10:18:09 +0300

    love-triangle: task is done

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
-};
+  let count = 0;
+  let length = preferences.length;
+  for (let i = 0; i < length; i++) {
+    let love1 = preferences[i];
+    let index_love1 = i + 1;
+    let index_love2 = love1 - 1;
+    if (index_love2 < length && index_love2 == love1 - 1) {
+      let love2 = preferences[index_love2];
+      let index_love3 = love2 - 1;
+      if (index_love3 < length && index_love3 == love2 - 1) {
+        let love3 = preferences[index_love3];
+        if (love3 == index_love1) {
+          count++;
+        }
+      }
+    }
+  }
+  return parseInt(count/3);
+}
